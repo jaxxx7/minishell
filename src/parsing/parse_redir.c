77b6bfec21dcebda65c057db28fe6d0c38d2c434
Yanis <yanisubu@DESKTOP-SIPLJ7A.localdomain>
2026-02-04 14:33:47 +0100

    fixed a bug command now stop at first error during redirection

--- src/parsing/parse_redir.c
+++ src/parsing/parse_redir.c
@@ -22,12 +22,15 @@ static void	handle_output_redir(t_cmd *cmd, t_token *token, int append)
 
 static void	handle_input_redir(t_cmd *cmd, char *filename)
 {
+	if (cmd->redir_error)
+		return ;
 	if (access(filename, F_OK) == -1)
 	{
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(filename, 2);
 		ft_putendl_fd(": No such file or directory", 2);
 		cmd->redir_error = 1;
+		return ;
 	}
 	free(cmd->infile);
 	cmd->infile = ft_strdup(filename);
